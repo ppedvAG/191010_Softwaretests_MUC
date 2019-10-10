2012839e038f7ec9c5fe4966d80a2e027bcfae76
Michael Zöhling <michael.zoehling@outlook.com>
2019-10-10 14:23:15 +0200

    Implementierung von Bankkonto erstellt

--- ÖsterreichischeZentralbank/ÖsterreichischeZentralbank/Bankkonto.cs
+++ ÖsterreichischeZentralbank/ÖsterreichischeZentralbank/Bankkonto.cs
@@ -8,27 +8,32 @@ namespace ÖsterreichischeZentralbank
 {
     public class Bankkonto
     {
-        private decimal neuerWert;
-
-        public Bankkonto()
-        {
-        }
+        public Bankkonto() : this(100m){}
 
         public Bankkonto(decimal neuerWert)
         {
-            this.neuerWert = neuerWert;
+            if (neuerWert < 0)
+                throw new ArgumentException();
+            Kontostand = neuerWert;
         }
 
-        public decimal Kontostand { get; set; }
+        public decimal Kontostand { get; private set; }
 
         public void Einzahlen(decimal betrag)
         {
-            throw new NotImplementedException();
+            if (betrag < 0)
+                throw new ArgumentException();
+            Kontostand += betrag;
         }
 
         public void Abheben(decimal betrag)
         {
-            throw new NotImplementedException();
+            if (betrag < 0)
+                throw new ArgumentException();
+            else if (Kontostand - betrag < 0)
+                throw new InvalidOperationException();
+
+            Kontostand -= betrag;
         }
     }
 }
