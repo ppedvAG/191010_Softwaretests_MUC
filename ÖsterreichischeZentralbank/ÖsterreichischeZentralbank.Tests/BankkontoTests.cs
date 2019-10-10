00b83c3bec9c9d56ee6dfa9739c98fbd4e318479
Michael Zöhling <michael.zoehling@outlook.com>
2019-10-10 13:48:50 +0200

    Init TDD

---
+++ ÖsterreichischeZentralbank/ÖsterreichischeZentralbank.Tests/BankkontoTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ÖsterreichischeZentralbank.Tests
+{
+    [TestClass]
+    public class BankkontoTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ ÖsterreichischeZentralbank/ÖsterreichischeZentralbank.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ÖsterreichischeZentralbank.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ÖsterreichischeZentralbank.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f8f4b7cb-36ac-46ea-a70b-3a14fe76502c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
