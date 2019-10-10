51043a546967742a15dac95ceb7b4a10b999dadc
Michael Zöhling <michael.zoehling@outlook.com>
2019-10-10 09:46:46 +0200

    "Hochkomplizierte" Logik für die ersten Tests erstellt

--- ErstenUnitTests/ErstenUnitTests/Taschenrechner.cs
+++ ErstenUnitTests/ErstenUnitTests/Taschenrechner.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace ErstenUnitTests
 {
-    public class Class1
+    public class Taschenrechner
     {
+        public int Add(int z1, int z2)
+        {
+            return z1 + z2;
+        }
     }
 }
