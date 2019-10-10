2bdc9db0bb1e3b154b0ab6ebd4fe30c35a05203e
Michael Zöhling <michael.zoehling@outlook.com>
2019-10-10 10:07:22 +0200

    Der erste Unit-Test

---
+++ ErstenUnitTests/ErstenUnitTests.Tests/TaschenrechnerTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ErstenUnitTests.Tests
+{
+    [TestClass]
+    public class TaschenrechnerTests
+    {
+        [TestMethod]
+        public void Add_5_and_3_returns_8()
+        {
+            // Arrange:
+            Taschenrechner tr = new Taschenrechner();
+
+            // Act:
+            var result = tr.Add(5, 3);
+
+            // Assert:
+            Assert.AreEqual(8, result);
+        }
+    }
+}
