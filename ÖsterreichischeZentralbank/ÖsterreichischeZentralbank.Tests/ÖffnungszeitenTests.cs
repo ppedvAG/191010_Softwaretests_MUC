342fb8bb032febb08264d67ff411f64861de71b3
Michael Zöhling <michael.zoehling@outlook.com>
2019-10-10 15:08:12 +0200

    Übung IsOpen

--- ÖsterreichischeZentralbank/ÖsterreichischeZentralbank.Tests/ÖffnungszeitenTests.cs
+++ ÖsterreichischeZentralbank/ÖsterreichischeZentralbank.Tests/ÖffnungszeitenTests.cs
@@ -11,11 +11,40 @@ namespace ÖsterreichischeZentralbank.Tests
     public class ÖffnungszeitenTests
     {
         [TestMethod]
-        [DataRow(2019,10,10,12,45,true)] // DO
-        public void IsOpen_Tests(int jahr,int monat, int tag, int stunde, int minute, bool erwartetesErgebnis)
+        [DataRow(2019, 10, 7, 10, 29, false)]   // Mo vor offen
+        [DataRow(2019, 10, 7, 10, 30, true)]    // Mo offen
+        [DataRow(2019, 10, 7, 18, 59, true)]    // Mo vor schluss
+        [DataRow(2019, 10, 7, 19, 00, false)]   // Mo schluss
+        [DataRow(2019, 10, 8, 10, 29, false)]   // Di vor offen
+        [DataRow(2019, 10, 8, 10, 30, true)]    // Di offen
+        [DataRow(2019, 10, 8, 18, 59, true)]    // Di vor schluss
+        [DataRow(2019, 10, 8, 19, 00, false)]   // Di schluss
+        [DataRow(2019, 10, 9, 10, 29, false)]   // Mi vor offen
+        [DataRow(2019, 10, 9, 10, 30, true)]    // Mi offen
+        [DataRow(2019, 10, 9, 18, 59, true)]    // Mi vor schluss
+        [DataRow(2019, 10, 9, 19, 00, false)]   // Mi schluss
+        [DataRow(2019, 10, 10, 10, 29, false)]  // Do vor offen
+        [DataRow(2019, 10, 10, 10, 30, true)]   // Do offen
+        [DataRow(2019, 10, 10, 18, 59, true)]   // Do vor schluss
+        [DataRow(2019, 10, 10, 19, 00, false)]  // Do schluss
+        [DataRow(2019, 10, 11, 10, 29, false)]  // Fr vor offen
+        [DataRow(2019, 10, 11, 10, 30, true)]   // Fr offen
+        [DataRow(2019, 10, 11, 18, 59, true)]   // Fr vor schluss
+        [DataRow(2019, 10, 11, 19, 00, false)]  // Fr schluss
+        [DataRow(2019, 10, 12, 10, 29, false)]  // Sa vor offen
+        [DataRow(2019, 10, 12, 10, 30, true)]   // Sa offen
+        [DataRow(2019, 10, 12, 13, 59, true)]   // Sa vor schluss
+        [DataRow(2019, 10, 12, 14, 00, false)]  // Sa schluss
+        [DataRow(2019, 10, 13, 10, 29, false)]  // So zu
+        [DataRow(2019, 10, 13, 10, 30, false)]  // So zu
+        [DataRow(2019, 10, 13, 18, 59, false)]  // So zu
+        [DataRow(2019, 10, 13, 19, 00, false)]  // So zu
+        public void IsOpen_Tests(int jahr, int monat, int tag, int stunde, int minute, bool erwartetesErgebnis)
         {
-            // Tipp:
+            Öffnungszeiten öz = new Öffnungszeiten();
             DateTime time = new DateTime(jahr, monat, tag, stunde, minute, 00);
+
+            Assert.AreEqual(erwartetesErgebnis, öz.IsOpen(time));
         }
     }
 }
--- ÖsterreichischeZentralbank/ÖsterreichischeZentralbank/Öffnungszeiten.cs
+++ ÖsterreichischeZentralbank/ÖsterreichischeZentralbank/Öffnungszeiten.cs
@@ -10,7 +10,38 @@ namespace ÖsterreichischeZentralbank
     {
         public bool IsOpen(DateTime date)
         {
-            throw new NotImplementedException();
+            TimeSpan startTime;
+            TimeSpan endTime  ;
+            switch (date.DayOfWeek)
+            {
+                case DayOfWeek.Monday:
+                case DayOfWeek.Tuesday:
+                case DayOfWeek.Wednesday:
+                case DayOfWeek.Thursday:
+                case DayOfWeek.Friday:
+                   startTime = new TimeSpan(10, 30, 00);
+                   endTime = new TimeSpan(19, 00, 00);
+
+                    if (date.TimeOfDay >= startTime &&
+                        date.TimeOfDay < endTime)
+                        return true;
+                    else
+                        return false;
+                case DayOfWeek.Saturday:
+                    startTime = new TimeSpan(10, 30, 00);
+                    endTime = new TimeSpan(14, 00, 00);
+
+                    if (date.TimeOfDay >= startTime &&
+                        date.TimeOfDay < endTime)
+                        return true;
+                    else
+                        return false;
+                case DayOfWeek.Sunday:
+                    return false;
+                default:
+                    throw new Exception("Hier sollte man eig ned reinkommen");
+            }
+
         }
     }
 }
