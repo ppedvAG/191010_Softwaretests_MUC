e023047164b4edef9ee077ca4cbe6be47bd6529e
Michael Zöhling <michael.zoehling@outlook.com>
2019-10-10 14:30:41 +0200

    Vorlage für Übung: Öffnungszeiten

---
+++ ÖsterreichischeZentralbank/ÖsterreichischeZentralbank.Tests/ÖffnungszeitenTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ÖsterreichischeZentralbank.Tests
+{
+    [TestClass]
+    public class ÖffnungszeitenTests
+    {
+        [TestMethod]
+        [DataRow(2019,10,10,12,45,true)] // DO
+        public void IsOpen_Tests(int jahr,int monat, int tag, int stunde, int minute, bool erwartetesErgebnis)
+        {
+            // Tipp:
+            DateTime time = new DateTime(jahr, monat, tag, stunde, minute, 00);
+        }
+    }
+}
---
+++ ÖsterreichischeZentralbank/ÖsterreichischeZentralbank/Öffnungszeiten.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ÖsterreichischeZentralbank
+{
+    public class Öffnungszeiten
+    {
+        public bool IsOpen(DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
