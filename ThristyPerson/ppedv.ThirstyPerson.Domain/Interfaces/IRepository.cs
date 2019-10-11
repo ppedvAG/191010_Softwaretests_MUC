f6e3cc7f0a423c7d4ec32beebdb41e92e8a9bf57
Michael Zöhling <michael.zoehling@outlook.com>
2019-10-11 13:47:18 +0200

    IRepository für die Datenbankschicht erstellt

---
+++ ThristyPerson/ppedv.ThirstyPerson.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ppedv.ThirstyPerson.Domain.Interfaces
+{
+    public interface IRepository
+    {
+        void Add<T>(T item) where T : Entity;
+        void Delete<T>(T item) where T : Entity;
+        void Update<T>(T item) where T : Entity;
+        T GetByID<T>(T item) where T : Entity;
+        IEnumerable<T> GetAll<T>() where T : Entity;
+
+        void Save();
+    }
+}
