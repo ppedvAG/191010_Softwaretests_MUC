0f130fb99ff4c3a511071a3e0c5bfe538b58e1b8
Michael Zöhling <michael.zoehling@outlook.com>
2019-10-11 11:46:06 +0200

    Im Core eine Logik erstellt, die die Maschine nutzt

--- ThristyPerson/ppedv.ThirstyPerson.Logic/Core.cs
+++ ThristyPerson/ppedv.ThirstyPerson.Logic/Core.cs
@@ -1,9 +1,30 @@
-﻿using System;
+﻿using ppedv.ThirstyPerson.Domain;
+using ppedv.ThirstyPerson.Domain.Interfaces;
+using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace ppedv.ThirstyPerson.Logic
 {
     public class Core
     {
+        public Core(IDevice device)
+        {
+            this.device = device;
+        }
 
+        private readonly IDevice device;
+
+        // Irgendeine Logik, die mit der Maschine was macht
+        // ---> Unittests für Core machen
+        public IEnumerable<Person> RecruitManyPersonsForCompany(int amount)
+        {
+            List<Person> newPersons = new List<Person>();
+            for (int i = 0; i < amount; i++)
+            {
+                newPersons.Add(device.RecruitPerson()); // Hier wird intern die Maschine genutzt
+            }
+            return newPersons;
+        }
     }
 }
