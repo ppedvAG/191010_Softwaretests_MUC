bac52dc13f4a5bfbee803bbace240f4453001314
Michael Zöhling <michael.zoehling@outlook.com>
2019-10-11 12:00:37 +0200

    Problemstellung: Testen mit der echten Maschine

---
+++ ThristyPerson/ppedv.ThirstyPerson.Logic.Tests/CoreTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ppedv.ThirstyPerson.Domain;
+using Robotech.Hardware;
+
+namespace ppedv.ThirstyPerson.Logic.Tests
+{
+    [TestClass]
+    public class CoreTests
+    {
+        // Ich will testen, ob am ende auch wirklich 5 Personen in der Liste rauskommen
+        // Problem 1: null (Maschine ist noch nicht im Büro)
+        [TestMethod]
+        public void RecruitManyPersonsForCompany_can_create_5_Persons_nullError()
+        {
+            Core core = new Core(null); // Problem: Gerät ist nicht da !!!
+
+            IEnumerable<Person> generatedPersons = core.RecruitManyPersonsForCompany(5);
+            generatedPersons.Should().HaveCount(5);
+        }
+
+        [TestMethod]
+        public void RecruitManyPersonsForCompany_can_create_5_Persons_with_real_Device()
+        {
+            Core core = new Core(new XINGRecruiter3000()); // Problem: Gerät ist nicht da !!!
+
+            IEnumerable<Person> generatedPersons = core.RecruitManyPersonsForCompany(5);
+            generatedPersons.Should().HaveCount(5);
+        }
+    }
+}
---
+++ ThristyPerson/ppedv.ThirstyPerson.Logic.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ppedv.ThirstyPerson.Logic.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ppedv.ThirstyPerson.Logic.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a0a477fe-3551-42e2-8cea-4dd72cf50348")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
